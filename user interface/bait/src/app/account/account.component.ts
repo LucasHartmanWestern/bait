388813c72e982a35ce99b5e0295f30965ee3fcee
LucasHartman <lucas.hartman@kognitive.ca>
2023-11-15 18:57:03 -0500

    Formatting fixes

--- user interface/bait/src/app/account/account.component.ts
+++ user interface/bait/src/app/account/account.component.ts
@@ -8,23 +8,5 @@ import { Component } from '@angular/core';
 export class AccountComponent {
 
   constructor() {
-    document.addEventListener('DOMContentLoaded', function() {
-      const carousel = document.querySelector('.carousel');
-      const cards = document.querySelectorAll('.card');
-      let scrollAmount = 0;
-
-      const cardWidth = cards[0].clientWidth;
-      const gap = 24; // Assuming a gap of 24px; adjust as per your design
-
-      document.querySelector('.carousel-control.right')?.addEventListener('click', () => {
-        if (carousel) carousel.scrollLeft += cardWidth + gap;
-      });
-
-      document.querySelector('.carousel-control.left')?.addEventListener('click', () => {
-        if (carousel) carousel.scrollLeft -= cardWidth + gap;
-      });
-    });
   }
-
-
 }
