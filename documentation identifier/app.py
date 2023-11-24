5d22948f9e5700dbdb6b53aedb71698f0856eae5
Ethan <epigou@uwo.ca>
2023-11-24 16:41:56 -0500

    Read documents as pdfs

--- documentation identifier/app.py
+++ documentation identifier/app.py
@@ -4,13 +4,27 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import nltk
 from nltk.corpus import stopwords
+import PyPDF2
 nltk.download('stopwords')
 
-documents = [
-    "Document 1 text goes here...",
-    "Document 2 text goes here...",
-    # Add more documents as needed
+pdf_file_paths = [
+    "TempDocs/(4000) Rebooting my Home Hub modem _ Reboot directly from the modem.pdf",
+    "TempDocs/How to stay connected during a power outage to your fibre-to-the-home phone service using the Giga Hub_Home Hub 4000.pdf",
+    "TempDocs/Manage Bell usernames and passwords _ Bell Wi-Fi and modem administrator.pdf",
+    "TempDocs/What is the Bell Wi-Fi app and how do I use it_ _ Block or pause Internet access by user.pdf",
+    # Add more paths as needed
 ]
+
+
+def read_pdf(file_path):
+    with open(file_path, 'rb') as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text() + "\n"
+        return text
+
+documents = [read_pdf(path) for path in pdf_file_paths]
 
 def find_best_match(documents, input_sentence):
     # Combine documents and the input sentence for vectorization
@@ -29,7 +43,7 @@ def find_best_match(documents, input_sentence):
     best_match_index = cosine_similarities.argsort()[0][-1]
     return documents[best_match_index]
 
-input_sentence = "Your input sentence here"
+input_sentence = "Parental controls"
 best_match = find_best_match(documents, input_sentence)
 print("Best matching document:", best_match)
 
