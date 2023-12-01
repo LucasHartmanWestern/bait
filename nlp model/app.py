0e65ceb7b4423b10301d0c45bbbad5077e727e76
DSagre <dhruvsagre252@gmail.com>
2023-12-01 17:36:07 -0500

    Demo NLP model

--- nlp model/app.py
+++ nlp model/app.py
@@ -1,2 +1,24 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from model_utils import load_model, generate_text
+from rbmodel import get_response
+
+app = Flask(__name__)
+CORS(app)
+
+print("############ APP STARTED ############")
+
+model_load_path = "bigram_language_model2.pt"
+loaded_model = load_model(model_load_path)
+
+print("############ MODEL LOADED ############")
+
+while True:
+    print("Hi, I am Bell's customer support, How may I assist you today?")
+    user_input = input("You: ")
+    print("Bot:", get_response(user_input))
+
 if __name__ == '__main__':
-    print("Running NLP Model")
+    print("Running NLP model")
+    app.run(debug=True, host='0.0.0.0')
+    
---
+++ nlp model/model_utils.py
@@ -0,0 +1,15 @@
+import torch
+from train import BigramLanguageModel, encode, decode, device
+
+# load the saved model
+def load_model(model_load_path):
+    device = torch.device('cpu')
+    loaded_model = BigramLanguageModel()
+    loaded_model.load_state_dict(torch.load('bigram_language_model2.pt', map_location=device))
+    loaded_model = loaded_model.to(device)
+    return loaded_model
+
+# generate some text
+def generate_text(loaded_model, max_new_tokens, starting_string='\n'):
+    context = torch.tensor([encode(starting_string)], dtype=torch.long, device=device)
+    return decode(loaded_model.generate(context, max_new_tokens=max_new_tokens)[0].tolist())
