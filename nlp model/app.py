7cdcf53d21c3b5c00c0cdb5fe10906d7d5711368
DSagre <dhruvsagre252@gmail.com>
2024-03-10 22:26:39 -0400

    CAP 134 Tweaked the model till it gave decently accurate responses

--- nlp model/app.py
+++ nlp model/app.py
@@ -2,13 +2,16 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from model_utils import load_model, generate_text
 from rbmodel import get_response
+from openai import OpenAI
+
+client = OpenAI(api_key=os.environ.get('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'))
 
 app = Flask(__name__)
 CORS(app)
 
 print("############ APP STARTED ############")
 
-model_load_path = "bigram_language_model2.pt"
+model_load_path = "test_model2.pt"
 loaded_model = load_model(model_load_path)
 
 print("############ MODEL LOADED ############")
