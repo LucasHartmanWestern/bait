52f593613922ef69b12501d30309ae41753b982b
DSagre <dhruvsagre252@gmail.com>
2024-03-19 02:12:46 -0400

    get pranked

--- user interface/bait/src/app/constants/constants.ts
+++ user interface/bait/src/app/constants/constants.ts
@@ -1,6 +1,6 @@
 //const serverPrefix = `https://bait-app-df10e84632ed.herokuapp.com`; // Heroku
 //const serverPrefix = 'http://127.0.0.1:8000'; // Localhost
-const serverPrefix = 'https://34.130.172.219:8000/'; // GCP
+const serverPrefix = 'https://brennanc.com'; // GCP
 
 export const Constants = {
   apiPaths: {
